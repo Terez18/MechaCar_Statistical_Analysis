c0dcf34b0d9363d6420245d9cdbbbf661c4ab4fd
Terez18 <tamarperez@comcast.net>
2021-05-16 18:49:26 -0700

    adding MechaCarChallenge.r

---
+++ R_Analysis/01_Demo/module_15.2.3_practice.r
@@ -0,0 +1,10 @@
+demo_table <- read.csv(file='demo.csv',check.names=F,stringsAsFactors = F)
+
+library(jsonlite)
+demo_table2 <- fromJSON(txt='demo.json')
+
+library(tidyverse)
+?mutate()
+
+
+?t.test()
